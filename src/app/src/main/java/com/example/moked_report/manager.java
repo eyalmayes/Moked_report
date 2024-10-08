1f6de85c1011466bbbfbb6bd54b8e6aa11331fce
Kobi mais <kobim@moked.local>
2024-10-08 22:59:19 +0300

    start working on Manager activity

--- src/app/src/main/java/com/example/moked_report/manager.java
+++ src/app/src/main/java/com/example/moked_report/manager.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class manager extends AppCompatActivity  {
@@ -14,6 +17,8 @@ public class manager extends AppCompatActivity  {
     TextView nameText;
     String userName;
     SharedPreferences sharedPreferences;
+    private static final int SQUARE_COUNT = 20;
+    private SquareView[] squareViews;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -27,6 +32,37 @@ public class manager extends AppCompatActivity  {
         sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         userName = sharedPreferences.getString("userName", null);
         nameText.setText(userName);
+
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+        squareViews = new SquareView[SQUARE_COUNT];
+
+        for (int i = 0; i < SQUARE_COUNT; i++) {
+            squareViews[i] = new SquareView(this);
+            final int index = i; // Needed for the click listener
+            squareViews[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onSquareClick(index);
+                }
+            });
+            gridLayout.addView(squareViews[i]);
+        }
+    }
+
+    private void onSquareClick(int index) {
+        // Handle square click
+        // Example: Change the color of the clicked square
+        squareViews[index].setBackgroundColor(Color.RED);
+    }
+
+    // Custom view for square
+    private class SquareView extends View {
+        public SquareView(manager context) {
+            super(context);
+            setLayoutParams(new LinearLayout.LayoutParams(250, 250)); // Set square dimensions
+            setBackgroundColor(Color.BLUE); // Initial color
+            setPadding(8, 8, 8, 8); // Add some padding
+        }
     }
 
 }
